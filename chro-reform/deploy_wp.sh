59814c0f4a665f9f8c4d4d1f5b11f8907ae21cd3
Truong Tam <tam@fudosan-king.jp>
2018-12-28 08:05:08 +0700

    Update link for contact

--- chro-reform/deploy_wp.sh
+++ chro-reform/deploy_wp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -rlpcgoDvz --delete --exclude-from=excludes ./wordpress/  php.intra.fudosan-king.jp:/var/www/chro-reform.com/wordpress/
+rsync -rlpcgoDvz --delete --exclude-from=excludes ./wordpress/wp-content/themes/chroniclereform/  php.intra.fudosan-king.jp:/var/www/chro-reform.com/wordpress/wp-content/themes/chroniclereform/

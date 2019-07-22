fa916249c0141f206583a5f25748de044d60d520
Khanh Nguyen <khanh@fudosan-king.jp>
2019-07-22 14:14:02 +0700

    update recruit page

--- propolife.co.jp/propolife_recruit/footer.php
+++ propolife.co.jp/propolife_recruit/footer.php
@@ -8,44 +8,44 @@
         <div class="naviBox">
             <ul class="naviUl clearfix">
                 <li class="heightLine-a" style="height: 372px;">
-                    <p><a href="https://propolife.co.jp/">ホーム</a></p>
+                    <p><a href="https://www.propolife.co.jp/">ホーム</a></p>
                 </li>
                 <li class="heightLine-a" style="height: 372px;">
-                    <p><a href="https://propolife.co.jp/news/">ニュース</a></p>
+                    <p><a href="https://www.propolife.co.jp/news/">ニュース</a></p>
                     <ul>
-                        <li><a href="https://propolife.co.jp/news/information/">企業からのお知らせ</a></li>
-                        <li><a href="https://propolife.co.jp/news/pressrelease/">プレスリリース</a></li>
-                        <li><a href="https://propolife.co.jp/news/media/">メディア掲載情報</a></li>
-                        <li><a href="https://propolife.co.jp/ir/">電子公告</a></li>
+                        <li><a href="https://www.propolife.co.jp/news/information/">企業からのお知らせ</a></li>
+                        <li><a href="https://www.propolife.co.jp/news/pressrelease/">プレスリリース</a></li>
+                        <li><a href="https://www.propolife.co.jp/news/media/">メディア掲載情報</a></li>
+                        <li><a href="https://www.propolife.co.jp/ir/">電子公告</a></li>
 
                     </ul>
                 </li>
                 <li class="heightLine-a" style="height: 372px;">
-                    <p><a href="https://propolife.co.jp/company/">企業情報</a></p>
+                    <p><a href="https://www.propolife.co.jp/company/">企業情報</a></p>
                     <ul>
-                        <li><a href="https://propolife.co.jp/company/president/">代表者挨拶</a></li>
-                        <li><a href="https://propolife.co.jp/company/philosophy/">経営理念</a></li>
-                        <li><a href="https://propolife.co.jp/company/">会社概要</a></li>
-                        <li><a href="https://propolife.co.jp/company/history/">沿革</a></li>
-                        <li><a href="https://propolife.co.jp/company/officer/">役員一覧</a></li>
-                        <li><a href="https://propolife.co.jp/company/access/">アクセス</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/president/">代表者挨拶</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/philosophy/">経営理念</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/">会社概要</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/history/">沿革</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/officer/">役員一覧</a></li>
+                        <li><a href="https://www.propolife.co.jp/company/access/">アクセス</a></li>
                     </ul>
                 </li>
                 <li class="heightLine-a" style="height: 372px;">
                     <p><a href="https://www.propolife.co.jp/recruit/" target="_blank">採用情報</a></p>
                 </li>
                 <li class="heightLine-a" style="height: 372px;">
-                    <p><a href="https://propolife.co.jp/group/">グループ企業</a></p>
+                    <p><a href="https://www.propolife.co.jp/group/">グループ企業</a></p>
                     <ul>
                         <li><a href="https://www.chronicle-web.com/" target="_blank">株式会社クロニクル</a></li>
-                        <li><a href="http://www.chronicle-kensetsu.co.jp/" target="_blank">株式会社クロニクル建設</a></li>
+                        <li><a href="https://www.chronicle-kensetsu.co.jp/" target="_blank">株式会社クロニクル建設</a></li>
                         <li><a href="https://www.prostyle-residence.com/" target="_blank">株式会社プロスタイル</a></li>
                         <li><span>株式会社プロスタイル横浜</span></li>
-                        <li><a href="http://chinokanri.co.jp/" target="_blank">千野建物管理株式会社</a></li>
+                        <li><a href="https://www.chinokanri.co.jp/" target="_blank">千野建物管理株式会社</a></li>
                         <li><span>煙台提案生活木業有限公司</span></li>
-                        <li><a href="http://www.propolifevietnam.com/" target="_blank">PROPOLIFE VIETNAM</a></li>
-                        <li><a href="http://nikuan-kotakino.com/" target="_blank">株式会社小滝野</a></li>
-<li><a href="http://www.propolifehotels.com">株式会社プロスタイル旅館</a></li>
+                        <li><a href="https://www.propolifevietnam.com/" target="_blank">PROPOLIFE VIETNAM</a></li>
+                        <li><a href="http://www.nikuan-kotakino.com/" target="_blank">株式会社小滝野</a></li>
+<li><a href="https://www.propolifehotels.com">株式会社プロスタイル旅館</a></li>
 <li><a href="https://www.oki-ig.com">株式会社沖縄イゲトー</a></li>
                     </ul>
                 </li>
@@ -53,16 +53,16 @@
                     <p><a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=lgqh-ockdr-75c9de3a9898c4710f57741e460fedab&amp;_ga=1.142172282.970461997.1490778452">お問い合わせ</a></p>
                 </li>
                 <li class="sp">
-                    <p><a href="https://propolife.co.jp/terms/">利用規約</a></p>
+                    <p><a href="https://www.propolife.co.jp/terms/">利用規約</a></p>
                 </li>
                 <li class="sp">
-                    <p><a href="https://propolife.co.jp/antisocial/">反社会的勢力排除に関する基本方針</a></p>
+                    <p><a href="https://www.propolife.co.jp/antisocial/">反社会的勢力排除に関する基本方針</a></p>
                 </li>
                 <li class="sp">
-                    <p><a href="https://propolife.co.jp/privacypolicy/">プライバシーポリシー</a></p>
+                    <p><a href="https://www.propolife.co.jp/privacypolicy/">プライバシーポリシー</a></p>
                 </li>
                 <li class="sp">
-                    <p><a href="https://propolife.co.jp/socialpolicy/">ソーシャルメディアポリシー</a></p>
+                    <p><a href="https://www.propolife.co.jp/socialpolicy/">ソーシャルメディアポリシー</a></p>
                 </li>
 
             </ul>
@@ -72,10 +72,10 @@
                 <p>Copyright © PROPOLIFE GROUP INC. All Rights Reserved.</p>
                 <ul class="clearfix">
                     <li><a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=lgqh-ockdr-75c9de3a9898c4710f57741e460fedab&amp;_ga=1.142172282.970461997.1490778452">お問い合わせ</a></li>
-                    <li><a href="https://propolife.co.jp/terms">利用規約</a></li>
-                    <li><a href="https://propolife.co.jp/antisocial">反社会的勢力排除に関する基本方針</a></li>
-                    <li><a href="https://propolife.co.jp/privacypolicy/">プライバシーポリシー</a></li>
-                    <li><a href="https://propolife.co.jp/socialpolicy/">ソーシャルメディアポリシー</a></li>
+                    <li><a href="https://www.propolife.co.jp/terms">利用規約</a></li>
+                    <li><a href="https://www.propolife.co.jp/antisocial">反社会的勢力排除に関する基本方針</a></li>
+                    <li><a href="https://www.propolife.co.jp/privacypolicy/">プライバシーポリシー</a></li>
+                    <li><a href="https://www.propolife.co.jp/socialpolicy/">ソーシャルメディアポリシー</a></li>
 
 
                 </ul>

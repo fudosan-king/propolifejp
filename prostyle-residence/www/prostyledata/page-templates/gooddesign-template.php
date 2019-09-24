3f30ebff5d27809d470fcdab57431c54e8d3a105
Khanh Nguyen PPL <khanh@fudosan-king.jp>
2019-09-24 16:37:38 +0700

    update prostyle data noindex

---
+++ prostyle-residence/www/prostyledata/page-templates/gooddesign-template.php
@@ -0,0 +1,243 @@
+<?php 
+/* Template Name: Good Design - Page Template */
+?>
+
+<?php get_header(); ?>
+
+<?php 
+
+if (have_posts()):
+    while (have_posts()): the_post();
+?>
+
+<main>
+    <section class="section_ryokan">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1><img src="<?php echo SGVinK::get_images_uri();?>/1x/g_type2019_f@2x.png" alt="" class="img-fluid"> <span>受賞</span></h1>
+                    <h2><span class="name">PROSTYLE</span> <span class="hotel">旅館</span> 横浜馬車道</h2>
+                    <div class="box_ryokan_topimg mx-auto text-center">
+                        <img src="<?php echo SGVinK::get_images_uri();?>/1x/fasade@2x.png" alt="" class="img-fluid">
+                        <p>当社が神奈川県横浜市中区常盤町で開発を行った 「プロスタイル旅館横浜馬車道」が、2019年度グッドデザイン賞 （主催：公益財団法人日本デザイン振興会）を受賞しました。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_concetp">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-center">
+                        <img src="<?php echo SGVinK::get_images_uri();?>/1x/asetto1@2x.png" alt="" class="img-fluid">
+                        <h2>Concept</h2>
+                        <h1>「まちなか旅館」<span>※ </span></h1>
+                    </div>
+                    <p class="description">プロスタイル旅館横浜馬車道は、数多くの歴史的洋館建造物が現存する横浜馬車道で、「まちなか旅館」をテーマに、畳や格子．障子といった日本の伝統的文化、街並みのノスタルジックな路地裏のバーを踏襲した雰囲気など、過去と現代、西洋と日本の和洋折衷を「心地よさ」と捉えて、現代的に再解釈した宿泊施設です。</p>
+                </div>
+            </div>
+        </div>
+        <div class="w_big_imgconcept">
+            <div class="big_imgconcept">
+                <img src="<?php echo SGVinK::get_images_uri();?>/1x/tatami@2x.png" alt="" class="img-fluid">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>日本はおもてなしの国である。旅館には安らぎの日本文化がある。国内外から多くの人が訪れ、にぎわう街中こそ旅館のもつ、おもてなしの文化をより感じ、安らぎを提供することができると考え建てる旅館。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w_concept_item">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <div class="concept_item">
+                            <a href="#" class="concept_item_img">
+                                <span class="num">1</span>
+                                <img src="<?php echo SGVinK::get_images_uri();?>/1x/gazoo11@2x.png" alt="" class="img-fluid">
+                            </a>
+                            <p>横浜馬車道の煉瓦舗装された道から引用したレンガ、 横浜馬車道が日本の近代街路樹の発祥地とされる ことから 引用した木を工夫しつつ用いた、周辺への調和だけでなく、新しさも融合させた外観。</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="concept_item">
+                            <a href="#" class="concept_item_img">
+                                <span class="num">2</span>
+                                <img src="<?php echo SGVinK::get_images_uri();?>/1x/gazoo13@2x.png" alt="" class="img-fluid">
+                            </a>
+                            <p>日旅人を静かに迎え入れる、半屋外の小径空問。馬車を連想するオブジェや、ガス灯をイメージさせる明かりを持つ。</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="concept_item">
+                            <a href="#" class="concept_item_img">
+                                <span class="num">3</span>
+                                <img src="<?php echo SGVinK::get_images_uri();?>/1x/gazoo12@2x.png" alt="" class="img-fluid">
+                            </a>
+                            <p>アプローチを抜けると、港町の路地裏にあるバ一を思わせるノスタルジックな空間。JAZZやBLUESの音色がやさしく包み込む。</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8 mx-auto">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="concept_item">
+                                    <a href="#" class="concept_item_img">
+                                        <span class="num">4</span>
+                                        <img src="<?php echo SGVinK::get_images_uri();?>/1x/gazoo14@2x.png" alt="" class="img-fluid">
+                                    </a>
+                                    <p>横浜馬車道の歴史的建造物の石造りをイメ ージする内装カラ一を用い、照度を抑えた明かりは横浜馬車道に日本初の ガス灯が灯った当時をほのかに感じられる空間。</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="concept_item">
+                                    <a href="#" class="concept_item_img">
+                                        <span class="num">5</span>
+                                        <img src="<?php echo SGVinK::get_images_uri();?>/1x/gazoo15@2x.png" alt="" class="img-fluid">
+                                    </a>
+                                    <p>くつを脱ぎ寛ぐことを誘発する小上がり、やさしく光や視線を通す格子や障子、ゆっくりと入浴できる洗い場付き浴槽などの日本文化の設えに、西洋文化のベッドや最新設備機器の利便性をなじませることで情緒的な価値を担保した空間。</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_outline">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-8 mx-auto">
+                    <div class="table_outline">
+                        <h2>受賞対象の概要</h2>
+                        <div class="row">
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>受賞対象名</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>ホテル [プロスタイル旅館 横浜馬車道]</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>受賞企業</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>株式会社プロスタイル（東京都）</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>事業主体名</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>株式会社プロポライフグループ</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>分類</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>14-02 商業のための建築・空間</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>概要</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>プロスタイル旅館横浜⾺⾞道は、数多くの歴史的洋館建造物が現存する横浜馬車道で、「まちなか旅館」を テーマに、畳や格⼦・障⼦といった⽇本的⽂化とノスタルジックな雰囲気漂う瀟洒な西洋の街並みが、 過去と現代、⻄洋と⽇本の和洋折衷を「⼼地よさ」と感じさせる現代的に再解釈した宿泊施設です。</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>プロデューサー</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>株式会社プロスタイル</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>ディレクター</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>株式会社プロスタイル</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>デザイナー</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>株式会社グリフォン</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>詳細情報</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p><a class="link_custom" target="_blank" href="https://www.prostyleryokan.com/yokohamabashamichi/">https://www.prostyleryokan.com/yokohamabashamichi/</a></p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>利用開始</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>2018年8月10日</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>価格</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>オープンプライス</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>販売地域</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>日本国内向け</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>設置場所</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>神奈川県横浜市中区常盤町5丁目64番地</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>仕様</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>◇用途／宿泊施設 ◇構造／RC造 ◇規模／地上10階建・地下１階 ◇敷地面積／421.49㎡ ◇建築面積／206.91㎡ ◇延床面積／2,987.69㎡ ◇客室数／94部屋</p>
+                            </div>
+                            <div class="col-12">
+                                <h2>受賞対象の詳細</h2>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>背景</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>オリンピックが近づき、ホテルが乱立する国内情勢のなか、日本の良さをより感じられるホテルが必要であると考えていた。そんな中ふと、街中を見回したら、同じようなシティ・ビジネスホテルが建っているだけで、地方田舎にある旅館が見当たらない。日本はおもてなしの国であり、旅館には安らぎの日本文化がある。日本文化と西洋文化が混ざった横浜馬車道は独自の文化を形成し、国内外から多くの人が訪れる観光地である。そのにぎわう街中こそ旅館を建てることで、日本のおもてなしや、心地よい安らぎを提供できると考え「まちなか旅館」を建てた。</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>経緯と成果</p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>街並みとの調和を意識したファサード、馬車を連想するオブジェを設置したアプローチ、どこかノスタルジックな雰囲気を感じるバーをイメージしたロビー、照度を抑えることでガス灯が灯った当時をほのかに感じられる落ち着いた廊下、日本文化の設えを取り込んだ客室へと、緩やかなシークエンスとなるように随所に地域性や日本文化を感じる要素を配置した。 ファサードは、街路樹の先駆けともいわれる馬車道を意識した木を感じるRC本実打放し仕上げや、街並みに残る赤レンガを模した素材の、貼り方を工夫した仕上げにすることにより、新しさを取り入れつつ、街並みとの融合を目指した。 客室は、くつを脱ぎ寛ぐことを誘発する小上がり、やさしく光や視線を通す格子や障子、ゆっくりと入浴できる洗い場付き浴槽などの日本文化の設えに、西洋文化のベッドや最新設備機器の利便性をなじませることで情緒的な価値を担保した。</p>
+                            </div>
+                            <div class="col-12 col-md-2 col_custom label_custom">
+                                <p>デザインの<span class="d-inline d-md-block">ポイント</span></p>
+                            </div>
+                            <div class="col-12 col-md-10 col_custom">
+                                <p>1. 横浜馬車道の歴史、街並みに関連するレンガや木を用いることで周辺との調和を目指したファサードデザイン。<br>
+                                    2. 日本的文化の設えを用いることで旅館のもつ「心地よい安らぎ」を感じるルームデザイン。<br>
+                                3. 馬車道を踏襲しつつ、ノスタルジックな雰囲気を持つ、アプローチ・ロビーデザイン。</p>
+                            </div>
+                            <div class="col-12 col_custom">
+                                <h2>審査員の評価</h2>
+                                <p>馬車道エリアの和洋折衷的なイメージや記憶、そして日本の伝統的空間性を、新たな旅館の定義にチャレンジすべく統合しようとしている姿勢がある。宿泊者の体験を印象深いものにするであろう細かな意匠的配慮も評価された。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php 
+        
+    endwhile;
+endif;
+
+?>
+
+<?php get_footer(); ?>

96f73f1f6a0870c513b537ec8e2e0cb68d6227f6
Junichi Tanabu <tanabu-junichi@propolife.co.jp>
2019-10-22 13:17:18 +0700

    #5481 update 次回更新予定日 2019/10/31 to 2019/11/31

--- prostyle-residence/www/nakanotetsugakudo/outline.php
+++ prostyle-residence/www/nakanotetsugakudo/outline.php
@@ -293,7 +293,7 @@
 
 
 
-					      		<p class="mb-5 text-center">情報提供日：<span>2019/9/30</span>　次回更新予定日：<span>2019/10/31</span></p>
+					      		<p class="mb-5 text-center">情報提供日：<span>2019/9/30</span>　次回更新予定日：<span>2019/11/31</span></p>
 
 	      					</div>
 	      				</div>

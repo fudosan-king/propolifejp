e29c71753bdf61c2a4f4ee9dcd2ecbf0f66da35e
Khanh Nguyen PPL <khanh@fudosan-king.jp>
2020-02-14 08:19:48 +0700

    update GTM for lp01 lp02 lp03 reservation

--- chronicle-web/reservation/head.php
+++ chronicle-web/reservation/head.php
@@ -22,3 +22,11 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.2.0/flickity.min.css">
 <link rel="stylesheet" href="css/styles.css" type="text/css">
 <link rel="stylesheet" href="css/mobile.css" type="text/css">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4CF54');</script>
+<!-- End Google Tag Manager -->

9988499a0ff5a6f4f44f353c1c8711715fee8617
Khanh Nguyen PPL <khanh@fudosan-king.jp>
2020-05-11 08:01:12 +0700

    オンライン接客予約ページ作成 -gtm tag add

--- chronicle-web/online/thanks/index.php
+++ chronicle-web/online/thanks/index.php
@@ -21,9 +21,20 @@
     <link rel="stylesheet" href="../assets/css/styles.css" type="text/css">
     <link rel="stylesheet" href="../assets/css/mobile.css" type="text/css">
     <script type="text/javascript" src="https://typesquare.com/3/tsst/script/ja/typesquare.js?5e72c6a675bc4b9598da5036e90393a3" charset="utf-8"></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W4CF54');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4CF54"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="page">
         <header>
             <div class="container">

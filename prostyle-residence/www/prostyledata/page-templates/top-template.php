6dc3cb4a3b2992ccf9b4a735584c33902a060dea
Khanh Nguyen PPL <khanh@fudosan-king.jp>
2020-07-13 11:55:39 +0700

    No. 122927 既存微修正_業務依頼 （プロスタイル　サイト）

--- prostyle-residence/www/prostyledata/page-templates/top-template.php
+++ prostyle-residence/www/prostyledata/page-templates/top-template.php
@@ -18,7 +18,7 @@ if (have_posts()):
 		<?php get_template_part( 'template-parts/home/block', 'news' ); ?>
 
 		<div class="block_valuable">
-			<div class="title"><img src="<?php SGVinK::the_images_uri(); ?>/lifevalue.png" class="img-responsive" alt="lifevalue"></div>
+			<div class="title">プロスタイルとは</div>
 			<?php the_content(); ?>
 
 			<br>
@@ -26,14 +26,16 @@ if (have_posts()):
 			<?php if (have_rows('brands_control')): ?>
 				<a href="#" class="btn btn_brand_commitment" data-toggle="modal" data-target="#modal_brands">
 					<!-- 新築マンションブランドのご紹介 <i class="i_down"></i> -->
-					<img src="<?php SGVinK::the_images_uri(); ?>/syoukai_button.png" class="img-responsive" alt="syoukai">
+					<!-- <img src="<?php //SGVinK::the_images_uri(); ?>/syoukai_button.png" class="img-responsive" alt="syoukai"> -->
+					プロスタイルのブランド展開
 				</a><br>
 			<?php endif; ?>
 
 			<?php if (have_rows('commitment_control')): ?>
 				<a href="#" class="btn btn_brand_commitment" data-toggle="modal" data-target="#modal_commitment">
 					<!-- こだわり <i class="i_down"></i> -->
-					<img src="<?php SGVinK::the_images_uri(); ?>/kodawari_button.png" class="img-responsive" alt="kodawari">
+					<!-- <img src="<?php //SGVinK::the_images_uri(); ?>/kodawari_button.png" class="img-responsive" alt="kodawari"> -->
+					こだわり
 				</a>
 			<?php endif; ?>
 

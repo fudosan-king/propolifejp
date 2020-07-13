17ede10015ec851413a92dbd6b92e20428ee9063
admin <huypv@propolife.co.jp>
2020-07-13 12:27:57 +0700

    No. 122927 既存微修正_業務依頼 （プロスタイル　サイト）

--- prostyle-residence/www/prostyledata/template-parts/footer/footer-js-content.php
+++ prostyle-residence/www/prostyledata/template-parts/footer/footer-js-content.php
@@ -9,7 +9,16 @@
 
 <script src="<?php SGVinK::the_js_uri(); ?>/bsnav.min.js"></script>
 <script src="<?php SGVinK::the_js_uri(); ?>/functions.js"></script>
-
+<script>
+      (function(d) {
+        var config = {
+          kitId: 'gih2che',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
 <?php 
 if(is_page( 'contact' )):
 	?>

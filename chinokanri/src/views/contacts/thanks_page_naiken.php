faf79952f01b801474a6cb02008fa90cc9662b56
Khiem <khiemtq@propolife.co.jp>
2020-07-21 15:15:01 +0700

    add naiken and chintai forms

---
+++ chinokanri/src/views/contacts/thanks_page_naiken.php
@@ -0,0 +1,19 @@
+<section class="section_thankspage">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>お問い合わせ</h1>
+                <ul>
+                    <li><a href="">１．ご入力</a></li>
+                    <li><a href="">２．ご確認</a></li>
+                    <li class="active"><a href="">３．完了</a></li>
+                </ul>
+                <div class="box_thanks_content">
+                    <h2>お問い合わせが完了しました</h2>
+                    <p>お問い合わせ有り難う御座います。<br>
+                    お手数ですが、<a href="tel:045-581-9556">045-581-9556</a>までお電話をお願い致します。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- chinokanri/src/views/templates/footer.php
+++ chinokanri/src/views/templates/footer.php
@@ -28,9 +28,7 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
         <script src="<?=base_url();?>assets/js/script.js"></script>
-        
         <?php 
             if($this->uri->uri_string() == 'contact'):
                 ?>
@@ -38,8 +36,14 @@
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.2/jquery.matchHeight-min.js"></script>
                 <script type="text/javascript" src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
                 <?php
-            endif;
-
+            endif; 
+            if($this->uri->uri_string()=='contact/naiken' || $this->uri->uri_string()=='contact/chintai'): ?>
+                <script src="<?=base_url();?>assets/js/naiken.js"></script>
+                <script type="text/javascript" src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.ja.min.js"></script>
+                <script src="<?=base_url();?>assets/js/functions.js"></script>
+            <?php endif;
             if($this->uri->uri_string() == 'kaiyaku'):
                 ?>
                 <script src="<?=base_url();?>assets/js/kaiyaku.js"></script>

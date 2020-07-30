c17cecd452b3fbd95179130f89cadcfbd0c3fd08
khiemtqfdk <khiemtq@propolife.co.jp>
2020-07-30 16:45:58 +0700

    http://redmine.fudosan-king.jp/issues/6956

--- prostyle-residence/www/prostyledata/template-parts/ir/content-listdoc.php
+++ prostyle-residence/www/prostyledata/template-parts/ir/content-listdoc.php
@@ -1,6 +1,5 @@
 <?php 
     if(is_page('ir')):
-
         if(have_rows('info_flexible_content')):
             $counter = 0;
             $field_object = get_field_object('info_flexible_content');
@@ -13,7 +12,7 @@
                     echo '<div class="row">';
                     ?>
                         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                            <h3><?php echo $text; ?></h3>
+                            <h3 class="p-title01"><?php echo $text; ?></h3>
                             <hr>
                         </div>
                         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -22,20 +21,27 @@
                                     echo '<div class="row">';
                                     while(have_rows('info_display')): the_row();
                                         $file = get_sub_field('file');
-                                        $fileSize = ' ['.size_format(filesize(get_attached_file( $file['ID']))).']';
-                                        $extImage = $file['subtype'] == 'pdf' ? SGVinK::get_images_uri()."ext/file_".$file['subtype'].".png" : $file['icon'];
-                                        ?>
-                                            <div class="col-6 col-sm-6 col-md-4 col-lg-2">
-                                                <article class="docItem">
-                                                    <a href="<?php echo $file['link']; ?>" target="_blank"><img src="<?php echo $extImage; ?>" class="img-fluid" alt=""></a>
-                                                    <div class="title"><?php echo $file['title'].$fileSize; ?></div>
-                                                    <div class="caption"><?php echo $file['caption'] ?></div>
-                                                    <div class="description"><?php echo $file['description'] ?></div>
-                                                </article>
+                                        if($file):
+                                            $fileSize = ' ('.size_format(filesize(get_attached_file( $file['ID']))).')';
+                                            $extImage = $file['subtype'] == 'pdf' ? SGVinK::get_images_uri()."ext/img_".$file['subtype'].".png" : $file['icon'];
+                                            ?>
+                                                <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                                                    <!-- <article class="docItem">
+                                                        <a href="<?php echo $file['link']; ?>" target="_blank"><img src="<?php echo $extImage; ?>" class="img-fluid" alt=""></a>
+                                                        <div class="title"><?php echo $file['title'].$fileSize; ?></div>
+                                                        <div class="caption"><?php echo $file['caption'] ?></div>
+                                                        <div class="description"><?php echo $file['description'] ?></div>
+                                                    </article> -->
+
+                                                    <a href="<?php echo $file['link']; ?>" target="_blank"> 
+                                                        <span class="p-text"><?php echo $file['title']; ?></span> 
+                                                        <img src="<?php echo $extImage; ?>" alt="PDF"> <?php echo $fileSize; ?>
+                                                    </a>
+                                                   
+                                               </div>
                                                
-                                           </div>
-                                           
-                                        <?php
+                                            <?php
+                                        endif;
                                     endwhile;
                                     echo '</div>';
                                 else:
@@ -50,13 +56,5 @@
                     echo $counter != $total_rows ? '<br><br>' : '';
                 endif;
             endwhile;
-            
         endif;
-
-    endif;
-?>
-
-
-    
-        
-    
+endif; ?>

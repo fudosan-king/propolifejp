c9f32a0711001f6a95d6c37dd3b6bc693bf331b6
Khanh Nguyen <khanh@propolife.co.jp>
2020-07-31 08:25:24 +0700

    update deploy prostyle-residence

--- prostyle-residence/www/deploy_prostyle_residence.sh
+++ prostyle-residence/www/deploy_prostyle_residence.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -rlpcgoDvz --delete --exclude-from=excludes_wp prostyledata/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/prostyledata/wp-content/themes/prostyledata
+rsync -rlpcgoDvz --delete --exclude-from=excludes_wp prostyledata/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/wordpress/wp-content/themes/prostyledata

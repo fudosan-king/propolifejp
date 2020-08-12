1e38659633c6ff4cfba88e19e3139b641baa1a6b
Huy Phan <huypv@propolife.co.jp>
2020-08-12 13:16:01 +0700

    No. 124015 既存微修正_業務依頼 （プレシス中野哲学堂パークフロント｜物件概要ページをWordpressで修正可能にする）

--- prostyle-residence/www/nakanotetsugakudo/outline.php
+++ prostyle-residence/www/nakanotetsugakudo/outline.php
@@ -79,204 +79,7 @@
 	      		<div class="main_outline">
 	      			<div class="container">
 	      				<div class="row">
-	      					<div class="col-12">
-	      						<h2 class="headline">OUTLINE</h2>
-					      		<h3 class="subheadline mt-5">「プレシス中野哲学堂パークフロント」物件概要</h3>
-
-					      		<table class="table table-bordered table_outline">
-					      			<tr>
-					      				<th>名称</th>
-					      				<td>プレシス中野哲学堂パークフロント</td>
-					      			</tr>
-					      			<tr>
-					      				<th>所在地</th>
-					      				<td>東京都中野区松が丘2丁目43番3（地番）、東京都中野区松が丘2丁目31-12（住居表示）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>交通</th>
-					      				<td>西武新宿線「新井薬師前」駅徒歩10分 <br>
-					      				都営地下鉄大江戸線「落合南長崎」駅（A1出口）徒歩13分 <br>JR総武・中央線・東京メトロ東西線「中野」駅よりバス7分、「哲学堂下」バス停より徒歩1分<br>JR山手線「新宿」駅よりバス25分、バス停「江古田一丁目」より徒歩2分<br>JR山手線「池袋」駅よりバス14分、バス停「江古田一丁目」より徒歩2分<br>西武池袋線「練馬」駅よりバス16分、バス停「江古田一丁目」より徒歩2分</td>
-					      			</tr>
-					      			<tr>
-					      				<th>地域・地区</th>
-					      				<td>第1種住居地域、防火地域、第3種高度地区、<br>
-					      				日影規制：4ｈ-2.5ｈ/4.0ｍ、最低敷地60m2、下水道処理区域</td>
-					      			</tr>
-					      			<tr>
-					      				<th>地目</th>
-					      				<td>宅地</td>
-					      			</tr>
-					      			<tr>
-					      				<th>建ペイ率</th>
-					      				<td>60％（70％）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>容積率</th>
-					      				<td>300%</td>
-					      			</tr>
-					      			<tr>
-					      				<th>敷地面積</th>
-					      				<td>582.06㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>建築面積</th>
-					      				<td>396.33㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>延床面積</th>
-					      				<td>1705.25㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>構造・規模</th>
-					      				<td>鉄筋コンクリート造、地上7階建</td>
-					      			</tr>
-					      			<tr>
-					      				<th>建築確認番号</th>
-					      				<td>第H29A-JK.eX00894-01号（平成29年10月19日付）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>計画変更建築確認番号</th>
-					      				<td>第JAIC2018X9111A2号（平成30年5月9日付）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>総戸数</th>
-					      				<td>26戸（他に管理員室1戸）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>駐車場</th>
-					      				<td>空きなし/総台数3台（機械式3台）（月額使用料：19,000円～22,000円）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>バイク置場</th>
-					      				<td>敷地内に5台（月額使用料：1,500円）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>分譲後の権利形態</th>
-					      				<td>専有部分は区分所有権、土地及び建物の共用部分は各区分所有権の<br>
-					      				専有面積割合による所有権の共有</td>
-					      			</tr>
-					      			<tr>
-					      				<th>売主・販売</th>
-					      				<td>株式会社プロスタイル<br>
-					      				東京都知事(1)第100343号<br>
-					      				（公社）首都圏不動産公正取引協議会加盟<br>
-					      				（公社） 東京都宅地建物取引業協会<br>
-					      				〒107-0061 東京都港区北⻘⼭3-6-23</td>
-					      			</tr>
-					      			<tr>
-					      				<th>設計・監理</th>
-					      				<td>株式会社オンズデコ　一級建築士事務所</td>
-					      			</tr>
-					      			<tr>
-					      				<th>施工会社</th>
-					      				<td>菊池建設株式会社</td>
-					      			</tr>
-					      			<tr>
-					      				<th>竣工時期</th>
-					      				<td>2019年1月10日（竣工済）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>入居時期</th>
-					      				<td>即入居可（各種手続完了後の入居となります）</td>
-					      			</tr>
-					      			<tr>
-					      				<th>設計図書閲覧場所</th>
-					      				<td>プレシス中野哲学堂パークフロント　現地販売センター</td>
-					      			</tr>
-					      			<tr>
-					      				<th>管理形態</th>
-					      				<td>区分所有者全員により管理組合を結成し管理を委託</td>
-					      			</tr>
-					      			<tr>
-					      				<th>管理会社</th>
-					      				<td>伏見管理サービス株式会社</td>
-					      			</tr>
-					      		</table>
-
-					      		<h3 class="subheadline mt-5">「プレシス中野哲学堂パークフロント」先着順販売概要</h3>
-					      		<table class="table table-bordered table_outline">
-					      			<tr>
-					      				<th>販売戸数</th>
-					      				<td>4戸</td>
-					      			</tr>
-					      			<tr>
-					      				<th>販売価格</th>
-					      				<td>5,290万円〜6,190万円</td>
-					      			</tr>
-					      			<tr>
-					      				<th>間取り</th>
-					      				<td>2LDK〜3LDK</td>
-					      			</tr>
-					      			<tr>
-					      				<th>住戸専有面積</th>
-					      				<td>52.91㎡〜64.07㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>バルコニー面積</th>
-					      				<td>3.75㎡〜6.39㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>室外機置場面積</th>
-					      				<td>1.75㎡ ～2.25㎡</td>
-					      			</tr>
-					      			<tr>
-					      				<th>管理費（月額）</th>
-					      				<td>19,385円〜22,765円　※インターネット・TV使用料含む</td>
-					      			</tr>
-					      			<tr>
-					      				<th>修繕積立金（月額）</th>
-					      				<td>4,230円〜5,130円</td>
-					      			</tr>
-					      			<tr>
-					      				<th>管理準備金（引渡時一括）</th>
-					      				<td>18,835円〜22,215円</td>
-					      			</tr>
-					      			<tr>
-					      				<th>修繕積立基金（引渡時一括）</th>
-					      				<td>283,410円〜343,710円</td>
-					      			</tr>
-					      			<tr>
-					      				<th>備考</th>
-					      				<td>※記載の販売価格は消費税率10％での税込み表示となります。</td>
-					      			</tr>
-					      			<tr>
-					      				<th>ローン概要</th>
-					      				<td>
-											<div class="mb-4">
-												<p>■支払い例Bタイプ【3LDK(54.86m²)】5590万円の場合</p>
-												<p>毎月の支払い額／144,369円</p>
-												<p>ボーナス時加算額／0円</p>
-												<p>頭金／0円</p>
-												<p>借入金の月々の元本／5590万円</p>
-												<p>借入金のボーナス払いの元本／0円</p>
-											</div>
-
-											<div class="mb-4">
-												<p>■支払い例 Fタイプ [2LDK（52.91㎡）] 5290万円の場合</p>
-												<p>毎月の支払い額／134,290円</p>
-												<p>ボーナス時加算額／0円</p>
-												<p>頭金／90万円</p>
-												<p>借入金の月々の元本／5200万円</p>
-												<p>借入金のボーナス払いの元本／0円</p>
-											</div>
-
-											<div class="">
-												<p>■ローンのご案内</p>
-												<p>提携ローン</p>
-												<p>提携金融機関／りそな銀行</p>
-												<p>販売価格に対する融資限度額の割合／100％以内</p>
-												<p>融資限度額／50万円以上１億円以内（10万円単位）</p>
-												<p>返済期間／1年～35年</p>
-												<p>利率／年利0.47％（変動金利）（店頭金利2.475%より全期間-2.005％）</p>
-												<p>融資事務手数料／お借入金額の2.2％</p>
-												<p>※ローンは一定要件該当者が対象です。適用される金利は融資実行時のものとなり、表記されている金利と異なる場合があります。</p>
-											</div>
-					      				</td>
-					      			</tr>
-					      		</table>
-
-					      		<p class="mb-5 text-center">更新日（8月11日）・更新予定日（8月25日）</p>
-
+	      					<div class="col-12" id="outline-content">
 	      					</div>
 	      				</div>
 	      			</div>
@@ -290,6 +93,17 @@
     </div>
 
     <?php require 'js-footer.php'; ?>
+    <script async="false">
+    	var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+		    if (this.readyState == 4 && this.status == 200) {
+		    	var response = JSON.parse(this.responseText)[0];
+		    	document.getElementById("outline-content").innerHTML = response.content.rendered
+		    }
+		  };
+		xhttp.open("GET", "/nakanotetsugakudo/wordpress/wp-json/wp/v2/pages?slug=outline", true);
+		xhttp.send();
+    </script>
 </body>
 <?php require 'gtm.php'; ?>
 </html>

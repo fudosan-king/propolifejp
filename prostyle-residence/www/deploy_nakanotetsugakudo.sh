a1c5ccc9c80cfc89cbb1b8aa9d9739b10949e8a4
Khanh Nguyen <khanh@propolife.co.jp>
2020-08-14 09:58:29 +0700

    update latest nakano + wp

--- prostyle-residence/www/deploy_nakanotetsugakudo.sh
+++ prostyle-residence/www/deploy_nakanotetsugakudo.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rsync -rlpcgoDvz --delete --exclude-from=excludes nakanotetsugakudo/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/
+rsync -rlpcgoDvz --delete --exclude-from=excludes_nakano nakanotetsugakudo/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/
+rsync -rlpcgoDvz --delete --exclude-from=excludes_wp nakanotetsugakudo/wordpress/wp-content/themes/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/wordpress/wp-content/themes/
--- prostyle-residence/www/feedback_nakanotetsugakudo.sh
+++ prostyle-residence/www/feedback_nakanotetsugakudo.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rsync -v -rlptgoDz --exclude-from=excludes_wp php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/ ./nakanotetsugakudo/
+rsync -v -rlptgoDz --exclude-from=excludes_nakano php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/ ./nakanotetsugakudo/
+rsync -v -rlptgoDz php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/wordpress/wp-content/themes/ ./nakanotetsugakudo/wordpress/wp-content/themes/

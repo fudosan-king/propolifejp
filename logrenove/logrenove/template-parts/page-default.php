687a44b681e091bdb8d4d431a09314d56d6d8830
Khanh Nguyen <khanh@propolife.co.jp>
2020-08-21 08:15:32 +0700

    quick update logrenove

--- logrenove/logrenove/template-parts/page-default.php
+++ logrenove/logrenove/template-parts/page-default.php
@@ -6,6 +6,8 @@
                     $thumbnails = new ThumbnailItem(get_post_thumbnail_id());
                     ?>
                         <h1><?php the_title(); ?></h1>
+                        
+                        <?php if(!is_preview()): ?>
                         <div class="box_about_main_img">
                             <?php if(!empty($thumbnails)): ?>
                                 <img src="<?php echo $thumbnails->url;?>" alt="<?php the_title(); //alt but get post title?>" title="<?php the_title(); //alt but get post title?>" class="img-fluid about_main_img">
@@ -13,6 +15,7 @@
                             <?php endif; ?>
                             <!-- <h2><img src="<?php //echo IMAGE_PATH; ?>/1x/logo_white.png" alt="logrenove_logo" class="img-fluid" width="149"></h2> -->
                         </div>
+                        <?php endif; ?>
                         <?php the_content(); ?>
                     <?php
                 endwhile;

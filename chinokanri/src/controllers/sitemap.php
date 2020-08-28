b38ecde8ca76c1ae795e2fe1477dca88eebede52
Khanh Nguyen <khanh@propolife.co.jp>
2020-08-28 11:57:05 +0700

    千野の問い合わせフォームについて外部からアクセスできないようにする。

---
+++ chinokanri/src/controllers/sitemap.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Sitemap extends CI_Controller {
+
+
+    /**
+     * Index Page for this controller.
+     *
+     */
+    public function index()
+    {
+        // $this->load->database();
+        // $query = $this->db->get("items");
+        // $data['items'] = $query->result();
+        $data['items'] = array(
+            'kaiyaku/',
+            'privacy-policy/',
+            'contact/',
+            // 'contact/naiken',
+            // 'contact/chintai',
+            // 'contact/kaiyaku',
+            // 'contact/bunjyo',
+            // 'contact/important',
+        );
+
+        $this->load->view('sitemap', $data);
+    }
+}
+
+/* End of file Home.php */
+/* Location: ./application/controllers/home.php */

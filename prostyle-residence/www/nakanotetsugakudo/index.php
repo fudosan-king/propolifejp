65323af68a62e8942040e4d16e69be3812936d6f
phuongnh88 <phuongnh88@propolife.co.jp>
2020-09-04 11:15:15 +0700

    No. 125273 既存微修正_業務依頼 （プレシス中野哲学堂パークフロント｜各ページ｜各種変更依頼）

--- prostyle-residence/www/nakanotetsugakudo/index.php
+++ prostyle-residence/www/nakanotetsugakudo/index.php
@@ -192,8 +192,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 							</ul>
 							<ul>
 								<li>
+                                    <p class="cornerunit">角住戸</p>
 									<h2>3LDK
-										<span class="liha">＋WIC<br>
+										<span>＋WIC<br>
 										＋SIC</span><br>
 									</h2>
 								</li>
@@ -261,7 +262,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                                     <p class="gold">2LDK 角住戸 <span>5,200</span>万円台～月々<span>13</span>万円台～</p>
                                 </li>
                                 <li>
-                                    <p class="gold">3LDK <span>5,600</span>万円台～月々<span>14</span>万円台～</p>
+                                    <p class="gold">3LDK 角住戸 <span>5,600</span>万円台～月々<span>14</span>万円台～</p>
                                 </li>
                             </ul>
 							<div class="box_large_modelroom">

9536876b15e2dd15e2aa25d83f93b695ebdfc41d
phuongnh88 <phuongnh88@propolife.co.jp>
2020-09-10 09:16:06 +0700

    No.120357 120341 120826 121061｜お問い合わせページ変更｜Back - Inportant Form - Config calendar

--- chinokanri/src/assets/js/pardot/customdate.js
+++ chinokanri/src/assets/js/pardot/customdate.js
@@ -3,7 +3,7 @@ $(function($) {
     var next_month_object = current.add(1, 'months');
     var next_month_fm = next_month_object.format('YYYY-MM-DD hh:mm');
     var end_of_month = moment(next_month_fm).endOf('month');
-    var day = next_month_object.add(1, 'days');
+    var day = next_month_object;
     if (day < end_of_month) {
         var day_off = day.format('YYYY-MM-DD hh:mm');
     } else {

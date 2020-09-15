f11f6f9a06ae71f131cd4fa56851661a9f45c3cf
phuongnh88 <phuongnh88@propolife.co.jp>
2020-09-15 12:00:13 +0700

    No.120357 120341 120826 121061｜お問い合わせページ変更｜Back

--- chinokanri/src/views/templates/footer.php
+++ chinokanri/src/views/templates/footer.php
@@ -27,6 +27,7 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-git.js"></script>
         <script src="<?=base_url();?>assets/js/script.js"></script>
         <?php 
             if($this->uri->uri_string() == 'contact'):

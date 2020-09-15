41e1c17c09806e27bf39218a0399c058fcc22866
Khanh Nguyen <khanh@propolife.co.jp>
2020-09-15 08:44:20 +0700

    update latest source chronicle kensetsu

--- chronicle-kensetsu/deploy.sh
+++ chronicle-kensetsu/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -rlpcgoDvz --delete --exclude-from=excludes ./ php.intra.fudosan-king.jp:/var/www/chronicle-kensetsu/
+rsync -rlpcgoDvz --delete --exclude-from=excludes ./src/ php.intra.fudosan-king.jp:/var/www/chronicle-kensetsu/

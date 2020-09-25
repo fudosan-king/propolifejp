46891cd7f31192002408c421d73e120daace5ce6
Khanh Nguyen <khanh@propolife.co.jp>
2020-09-25 16:19:56 +0700

    update shell prostyle-residence nakano 2

--- prostyle-residence/staging_deploy_nakanotetsugakudo.sh
+++ prostyle-residence/staging_deploy_nakanotetsugakudo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rsync -rlpcgoDvz --delete --exclude-from=./www/excludes_nakano ./www/nakanotetsugakudo/ dev.php.intra.fudosan-king.jp:/var/www/prostyle-residence/nakanotetsugakudo/
-rsync -rlpcgoDvz --delete --exclude-from=./www/excludes_wp ./www/nakanotetsugakudo/wordpress/wp-content/themes/ php.intra.fudosan-king.jp:/var/www/prostyle-residence.com/nakanotetsugakudo/wordpress/wp-content/themes/
+rsync -rlpcgoDvz --delete --exclude-from=./www/excludes_wp ./www/nakanotetsugakudo/wordpress/wp-content/themes/ dev.php.intra.fudosan-king.jp:/var/www/prostyle-residence/nakanotetsugakudo/wordpress/wp-content/themes/

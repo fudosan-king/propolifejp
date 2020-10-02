e190f4c22061e4cf658432322fdd7653d5e607cb
Khanh Nguyen <khanh@propolife.co.jp>
2020-10-02 09:51:54 +0700

    update source prostyleryokan 2

--- prostyleryokan.com/prostyleryokan/header.php
+++ prostyleryokan.com/prostyleryokan/header.php
@@ -39,8 +39,16 @@
 
     <?php wp_head(); ?>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WD2TVMK');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="<?php echo TEMPLATE_ASSETS_PATH; ?>/gtag/js%3Fid=UA-153074140-1"></script>
+    <!-- <script async src="<?php //echo TEMPLATE_ASSETS_PATH; ?>/gtag/js%3Fid=UA-153074140-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -51,10 +59,15 @@
 
         gtag('config', 'UA-153074140-1');
 
-    </script>
+    </script> -->
 </head>
 
 <body class="p-home">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WD2TVMK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <header id="js-gHeaderWrap" class="gHeaderWrap <?php echo !is_front_page() ? 'header_underpage' : ''; ?>">
         <div class="gHeader">

656cb2ef5273e2c4638c55994a5a027de6d6322d
khiemtq <khiemtq>
2020-10-08 10:00:20 +0700

    http://redmine.fudosan-king.jp/issues/7402

---
+++ logrenove/logrenove/assets/js/form/mailmagazine.js
@@ -0,0 +1,139 @@
+$(function($) {
+
+    $('#btnAgree').on('click',function(e){
+        var frm_data = $('form.frm_registration').serializeArray();
+        var isValid = check_valid(frm_data);
+        if(isValid === false) {
+            $('html').animate({
+                scrollTop: $('.frm_registration').offset().top
+            }, 1000, function() {});
+        }
+        else
+        {
+            goConfirm(frm_data);
+        }
+        e.preventDefault();
+        return false;
+    });
+
+    $('#btnBack').on('click',function(e){
+        goBack();
+        return false;
+    });
+
+    function goConfirm(data)
+    {
+        $('.frm-input').fadeOut();
+        $('.frm_confirm').fadeIn(function() {
+            $('html').scrollTop($('.frm_registration').offset().top);
+        });
+
+        var tmpName = '';
+        $.each(data, function(key, value) {
+            var name = value.name;
+            var val = value.value;
+            if (name.indexOf('[]') == -1) 
+            {
+                $("#"+name).text(val);
+            }
+            else 
+            {
+                name = name.replace('[]','');
+                if(tmpName != name){
+                    $("#"+name).html(escapeHtml(val) + '<br>');
+                    tmpName = name;
+                }else{
+                    $("#"+name).append(escapeHtml(val) + '<br>');
+                }
+                
+            }
+        })
+    }
+
+    function goBack()
+    {
+        $('.frm-input').fadeIn(function() {
+            $('html').scrollTop($('.frm_registration').offset().top);
+        });
+        $('.frm_confirm').fadeOut();
+        $('.frm-input').find('input, select').removeClass('validate-error');
+    }
+
+    function isEmail(email) {
+      var emailPattern = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      return emailPattern.test(email);
+    }
+
+    function check_valid() {
+        var ERROR_NO_INPUT = '値を入力してください';
+        var data = $('.frm-input input.required,select.required').map(function() {
+            key = $(this).attr('name');
+            val = $(this).val();
+            type_elem = $(this).attr('type');
+            if(['radio'].indexOf(type_elem) != -1) {
+                checked = $('input[name='+key+']:checked');
+                val = checked.length > 0 ? checked.val() : '';
+            }
+            else if(['checkbox'].indexOf(type_elem) != -1) {
+                key = key.indexOf('[]') != -1 ? key.replace('[]','') : key;
+                val = $('input[name^='+key+']:checked').map(function() {
+                    return $(this).val();
+                });
+            }
+            var obj = {};
+            obj['name'] = key;
+            obj['value'] = val;
+            return obj;
+        });
+        var invalid = $();
+        $.each(data, function(key, value) {
+            var name = value.name;
+            var val = value.value;
+            var element = $('[name^='+name+']');
+            var required = element.hasClass('required');
+            if(required) {
+                if(val == '' || val.length == 0) {
+                    element.addClass('validate-error');
+                    invalid.push(name);
+
+                }
+                else if(name == 'email') {
+                    if(!isEmail(val))
+                    {
+                        element.addClass('validate-error');
+                        invalid.push(name);
+                    }
+                    else {
+                        element.removeClass('validate-error');
+                        // element.addClass('is-valid');
+                    }
+                }
+                else {
+                    element.removeClass('validate-error');
+                    // element.addClass('is-valid');
+                }
+            }
+        });
+        if(invalid.length > 0) return false;
+        else return true;
+    }
+
+    function escapeHtml(text) {
+      return text
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/"/g, "&quot;")
+          .replace(/'/g, "&#039;");
+    }
+})
+
+var demo = function() {
+        $('input[name="name"]').val('test');
+        $('input[name="email"]').val('test@gmail.com');
+        $('textarea[name="inquiry_content"]').val('test');
+
+        setTimeout(function(){
+            // $('#btn_confirm').click();
+        },2000);
+}

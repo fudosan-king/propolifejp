fa2a91a71bf6b041382db606911b91be79a45324
khiemtq <khiemtq>
2020-10-26 09:44:15 +0700

    7457-fix_cols ranking

--- logrenove/logrenove/template-parts/sidebar/ranking-posts.php
+++ logrenove/logrenove/template-parts/sidebar/ranking-posts.php
@@ -8,13 +8,13 @@
         ?>
                         <article class="article_items">
                             <div class="row no-gutters">
-                                <div class="col-6 align-self-center">
+                                <div class="col-4">
                                     <a href="<?php echo $post->permalink; ?>" class="article_items_img">
                                         <img data-src="<?php echo $post->thumbails_url;?>" alt="<?php echo $post->title;?>" title="<?php echo $post->title;?>" class="img-fluid">
                                     </a>
                                     <span class="article_ranking ranking-<?php echo $post->rank;?>"><?php echo $post->rank;?></span>
                                 </div>
-                                <div class="col-6 align-self-center">
+                                <div class="col-8">
                                     <div class="article_items_content">
                                         <p class="article_items_name"><a href="<?php echo $post->permalink; ?>" ><?php echo $post->title; ?></a></p>
                                         <span class="badge badge-secondary badge_cate"><?php echo $post->firstCat;?></span>

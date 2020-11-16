263130b87d4cedbd93084c5ccdd9440c8110cabb
Bao Chan <chandhb@propolife.co.jp>
2020-11-16 16:42:27 +0700

    http://redmine.fudosan-king.jp/issues/7559

--- prostyleryokan.com/kenchomae/template-part/page/home/concept.php
+++ prostyleryokan.com/kenchomae/template-part/page/home/concept.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="box_concept">
-					<div class="row flex-column-reverse flex-md-row">
+					<div class="row flex-column-md-reverse flex-md-row">
 						<div class="col-12 col-md-7">
 							<!-- <img src="<?php _echo($data->background->url); ?>" alt="" class="w-100 h-100" style="object-fit: cover;"> -->
 							<div 

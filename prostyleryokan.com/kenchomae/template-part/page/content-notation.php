03fbba00b8767e935e868d1d3264c9bd019eb27a
Bao Chan <chandhb@propolife.co.jp>
2020-12-17 14:20:00 +0700

    ◇No. 131237 開発依頼 （プロスタイル旅館那覇県庁前ページの作成）

--- prostyleryokan.com/kenchomae/template-part/page/content-notation.php
+++ prostyleryokan.com/kenchomae/template-part/page/content-notation.php
@@ -10,19 +10,20 @@
 		</div>
 	</div>
 </section>
-
 <div class="main_content">
-	<section class="section_social_media_policy mb-0">
+	<section class="section_termofuse mb-0">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					<?php 
-						if(have_posts()):
-							while(have_posts()): the_post();
-								the_content();
-							endwhile;
-						endif;
-					?>
+					<div class="box_termofuse">
+						<?php 
+							if(have_posts()):
+								while(have_posts()): the_post();
+									the_content();
+								endwhile;
+							endif;
+						?>
+					</div>
 				</div>
 			</div>
 		</div>

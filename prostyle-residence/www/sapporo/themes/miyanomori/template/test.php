70c1125a880782b5d9f785796fc92c0e26c74f36
Anh Thi <anhthi92@propolife.co.jp>
2020-12-17 15:09:04 +0700

    No. 125141 新規作成・リニュアル_業務依頼 （プロスタイル宮の森(仮)販売サイト作成) | front& back

--- prostyle-residence/www/sapporo/themes/miyanomori/template/test.php
+++ prostyle-residence/www/sapporo/themes/miyanomori/template/test.php
@@ -5,15 +5,12 @@ Template Post Type: page
 */
 ?>
 
+<?php get_header(); ?>
 
 <div class="container">
-
-<h1>TEST</h1>
-
-<?php echo do_shortcode("[apartment_plan]"); ?>
-
+	<h1>TEST</h1>
+	<?php echo do_shortcode("[apartment-plan]"); ?>
 </div>
-
 
 <?php get_template_part( 'template/home/footer'); ?>
 <?php get_footer(); ?>

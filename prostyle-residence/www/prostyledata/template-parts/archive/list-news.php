58973dd8f3d732e688a1ae41da908d7829b6041d
Khanh Nguyen <khanh@propolife.co.jp>
2020-12-22 10:15:03 +0700

    Hotfix_20201221: prostyle-residence news part has a problem

--- prostyle-residence/www/prostyledata/template-parts/archive/list-news.php
+++ prostyle-residence/www/prostyledata/template-parts/archive/list-news.php
@@ -13,22 +13,24 @@
                 <?php 
                 if(have_posts()):
                     while(have_posts()): the_post();
-                        $thumnailImage = wp_get_attachment_image_url( get_post_thumbnail_id(), $size = 'large', $icon = false );
-                        ?>
-                            
-                            <div class="news-data">
-                                <div class="xcol dt-1">
-                                    <?php echo date('Y-m-d ', get_post_time()); ?>
-                                </div>
-                                <div class="xcol dt-2">
-                                    <?php echo get_taxonomy_custom_post_string( $post, 'news-cat' ); ?>
-                                </div>
-                                <div class="xcol dt-3">
-                                    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title();  ?></a>
-                                </div>
-                            </div>               
+                        if(get_post_status() == 'publish'):
+                            $thumnailImage = wp_get_attachment_image_url( get_post_thumbnail_id(), $size = 'large', $icon = false );
+                            ?>
+                                
+                                <div class="news-data">
+                                    <div class="xcol dt-1">
+                                        <?php echo date('Y-m-d ', get_post_time()); ?>
+                                    </div>
+                                    <div class="xcol dt-2">
+                                        <?php echo get_taxonomy_custom_post_string( $post, 'news-cat' ); ?>
+                                    </div>
+                                    <div class="xcol dt-3">
+                                        <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title();  ?></a>
+                                    </div>
+                                </div>               
 
-                        <?php
+                            <?php
+                        endif;
                     endwhile;
 
                     ?>

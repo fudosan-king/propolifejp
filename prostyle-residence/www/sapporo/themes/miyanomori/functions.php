ce63990bff63989b71aec75dbde681c2089c920d
dongpt93 <dongpt93@propolife.co.jp>
2020-12-24 09:41:08 +0700

    No. 125141 新規作成・リニュアル_業務依頼 （プロスタイル宮の森(仮)販売サイト作成）| back end

--- prostyle-residence/www/sapporo/themes/miyanomori/functions.php
+++ prostyle-residence/www/sapporo/themes/miyanomori/functions.php
@@ -196,7 +196,25 @@ function miyanomori_blogs_type($atts){
 }
 
 
+/**
+ * Generates custom logout URL
+ */
+function getLogoutUrl($redirectUrl = ''){
+    if(!$redirectUrl) $redirectUrl = site_url();
+    $return = str_replace("&amp;", '&', wp_logout_url($redirectUrl));
+    return $return;
+}
 
+/**
+ * Bypass logout confirmation on nonce verification failure
+ */
+function logout_without_confirmation($action, $result){
+    if(!$result && ($action == 'log-out')){ 
+        wp_safe_redirect(getLogoutUrl()); 
+        exit(); 
+    }
+}
+add_action( 'check_admin_referer', 'logout_without_confirmation', 1, 2);
 
 // add_action( 'admin_enqueue_scripts', 'miyanomori_admin_enqueue' );
 

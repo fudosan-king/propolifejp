8dbf9ef9e69328ecc2290ab17f4744a00a639ad1
Anh Thi <anhthi92@propolife.co.jp>
2021-01-22 10:44:01 +0700

    No. 125141 新規作成・リニュアル_業務依頼 （プロスタイル宮の森(仮)販売サイト作成) | front& back

--- prostyle-residence/www/sapporo/themes/miyanomori/template/about.php
+++ prostyle-residence/www/sapporo/themes/miyanomori/template/about.php
@@ -11,13 +11,13 @@ Template Post Type: page
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-12">
-                    <h2 class="title_sub"><?p= _e('価値あるものと暮らす幸せを。','miyanomori'); ?></h2>
-                    <p class="text-center"><?p= _e('人生は、つねに「住まい」とともにあります。','miyanomori'); ?><br>
-                        <?p= _e('いうなれば「住まい」はその人の生き方そのもの。','miyanomori'); ?><br>
-                        <?p= _e('人それぞれ、十人十色のスタイルがあるなか、','miyanomori'); ?><br>
-                        <?p= _e('私たちは「住」のプロフェッショナルとして','miyanomori'); ?><br>
-                        <?p= _e('お客様に最適な「住まい」、','miyanomori'); ?><br>
-                        <?p= _e('そしてライフスタイルを提案していきます。','miyanomori'); ?></p>
+                    <h2 class="title_sub"><?= _e('価値あるものと暮らす幸せを。','miyanomori'); ?></h2>
+                    <p class="text-center"><?= _e('人生は、つねに「住まい」とともにあります。','miyanomori'); ?><br>
+                        <?= _e('いうなれば「住まい」はその人の生き方そのもの。','miyanomori'); ?><br>
+                        <?= _e('人それぞれ、十人十色のスタイルがあるなか、','miyanomori'); ?><br>
+                        <?= _e('私たちは「住」のプロフェッショナルとして','miyanomori'); ?><br>
+                        <?= _e('お客様に最適な「住まい」、','miyanomori'); ?><br>
+                        <?= _e('そしてライフスタイルを提案していきます。','miyanomori'); ?></p>
                 </div>
             </div>
         </div>

56cdadac35e65d94fa95d91f0f83ef6d289e5cf4
khiemtq <khiemtq@propolife.co.jp>
2021-01-25 09:39:16 +0700

    add excludes for logrenove rsync

--- logrenove/staging_deploy.sh
+++ logrenove/staging_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync --delete -rlpcgz -v ./logrenove/ dev.php.intra.fudosan-king.jp:~/www/logrenove/wp-content/themes/logrenove/
+rsync --delete -rlpcgz --exclude-from=excludes -v ./logrenove/ dev.php.intra.fudosan-king.jp:~/www/logrenove/wp-content/themes/logrenove/
